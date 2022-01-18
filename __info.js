6f418eb7e2b33061d84e3cc9846f9f8161d9e4dc
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2022-01-17 19:31:42 -0500

    ⚡️ feat: start gatekeeping

--- __info.js
+++ __info.js
@@ -1,5 +1,10 @@
 export const name = "3D";
 
+/** @type {import("../..").Gatekeep} */
+export const gatekeep = async () => {
+	return { able: true };
+};
+
 /** @typedef {{}} Options */
 
 /** @type {import("../..").AdderOptions<Options>} */
